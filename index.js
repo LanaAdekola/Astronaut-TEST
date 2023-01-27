a180314e5476dcf70d1d2fc0be0d24e666f81f54
LanaAdekola <adekolaadelana@gmail.com>
2023-01-27 10:08:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const loginForm = document.getElementById('login-form')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
+
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -17,3 +19,16 @@ consentForm.addEventListener('submit', function(e){
         <img src="images/loading.svg" class="loading">
         <p id="upload-text">Uploading your data to the dark web...</p>
     </div>` 
+
+    setTimeout(function(){
+        document.getElementById('upload-text').innerText = "Making the sale..."
+    }, 1500)
+    
+    setTimeout(function(){
+document.getElementById('modal-inner').innerHTML=    `
+    <h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>`
+    }, 3000)
