f753ad163a514bcbc2c901380c7f3bcd25c88573
LanaAdekola <adekolaadelana@gmail.com>
2023-01-27 09:38:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const loginForm = document.getElementById('login-form')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
 
-loginForm.addEventListener('submit', function(e){
-    e.preventDefault()
+setTimeout(function(){
+    modal.style.display = 'inline'
+}, 1500)
+
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
 })
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>` 
