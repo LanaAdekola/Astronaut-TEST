fc38d75f0c39d67b906a0fbc0d816e75716f0498
LanaAdekola <adekolaadelana@gmail.com>
2023-02-08 11:58:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const loginForm = document.getElementById('login-form')
 
-loginForm.addEventListener('submit', function(){
- 
+loginForm.addEventListener('submit', function(e){
+    e.preventDefault()
     const loginFormData = new FormData(loginForm)
 
 })
