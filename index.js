28bd45a51463789e6267de60bbc1be12ba2e3d1f
LanaAdekola <adekolaadelana@gmail.com>
2023-03-14 19:57:13 +0100

    final

--- index.js
+++ index.js
@@ -3,5 +3,20 @@ const loginForm = document.getElementById('login-form')
 loginForm.addEventListener('submit', function(e){
     e.preventDefault()
     const loginFormData = new FormData(loginForm)
-clearInterval()
+
+    const name = loginFormData.get('astronautName')
+    const email = loginFormData.get('astronautEmail')
+    const password = loginFormData.get('astronautPassword')
+    console.log(name, email, password) 
+    clear()
 })
+function clear(){
+    const name = document.getElementById('astronautName')
+    const email = document.getElementById('astronautEmail')
+    const password = document.getElementById('astronautPassword')
+
+name.value = ''
+email.value = ''
+password.value = ''
+}
+
